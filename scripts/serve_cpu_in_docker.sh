8869816491963819b7e4252552b683825f052ef7
Zhengkai Tu <ztu@mit.edu>
2023-11-01 15:48:19 -0400

    syntax change for helm

--- scripts/serve_cpu_in_docker.sh
+++ scripts/serve_cpu_in_docker.sh
@@ -5,5 +5,4 @@ fi
 docker run -d --rm \
   --name mcts \
   --network=host \
-  -t ${ASKCOS_REGISTRY}/tree_search/mcts:1.0-cpu \
-  python mcts_server.py
+  -t ${ASKCOS_REGISTRY}/tree_search/mcts:1.0-cpu
